55545c7b575da67553b5aea9faf5bdd91f72490c
David-ZemaP <david.zema@ucb.edu.bo>
2025-05-12 16:52:54 -0400

    Establecer rutas de los paginas creadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './paginas/inicio/inicio.component';
+import { ContactanosComponent } from './paginas/contactanos/contactanos.component';
+import { NosotrosComponent } from './paginas/nosotros/nosotros.component';
+import { TiendaComponent } from './paginas/tienda/tienda.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: InicioComponent },
+    { path: 'inicio', component: InicioComponent },
+    { path: 'contactanos', component: ContactanosComponent },
+    { path: 'nosotros', component: NosotrosComponent },
+    { path: 'tienda', component: TiendaComponent },    
+];
