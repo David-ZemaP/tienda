9579c9995a22236db6841a3552ac15a3292d1df9
David-ZemaP <david.zema@ucb.edu.bo>
2025-05-12 17:49:19 -0400

    Se añadio rutas para pagina no encontrada y para la ruta por defecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { InicioComponent } from './paginas/inicio/inicio.component';
 import { ContactanosComponent } from './paginas/contactanos/contactanos.component';
 import { NosotrosComponent } from './paginas/nosotros/nosotros.component';
 import { TiendaComponent } from './paginas/tienda/tienda.component';
+import { PaginaNoEncontradaComponent } from './paginas/pagina-no-encontrada/pagina-no-encontrada.component';
 
 export const routes: Routes = [
     { path: 'inicio', component: InicioComponent },
-    { path: '', component: InicioComponent },
     { path: 'contactanos', component: ContactanosComponent },
     { path: 'nosotros', component: NosotrosComponent },
     { path: 'tienda', component: TiendaComponent },    
+    { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+    { path: '**', component: PaginaNoEncontradaComponent, pathMatch: 'full' } 
 ];
